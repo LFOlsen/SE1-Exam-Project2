c0bdddb02ada0e2a0843623bf9f2170ac0514668
Rastin Matin <rastinmatin@gmail.com>
2020-04-09 13:57:44 +0200

    Added further comments...

--- src/dtu/planning/model/NotificationType.java
+++ src/dtu/planning/model/NotificationType.java
@@ -2,6 +2,7 @@ package dtu.planning.model;
 
 public class NotificationType {
     /**
-     * Notification-types for the view-controller
+     * Notification-types for the view-controller.
+     * Might consider merging this with the app-folder in the end?
      */
 }
